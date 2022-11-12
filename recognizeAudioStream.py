27f6c84bd906dbe515dd298d28fda590bf9c82eb
RonyMacfly <rony.macfly@gmail.com>
2022-11-12 12:12:29 -0500

    recognizeAudioStream

---
+++ recognizeAudioStream.py
@@ -0,0 +1,51 @@
+import pyaudio
+import wave
+import json
+
+'''
+task manager
+
+pulseaudio --daemonize=no [kill]
+'''
+
+def getText(text):
+	res = json.loads(text)
+	return res["text"]
+
+from voskLocal import Model, KaldiRecognizer, SetLogLevel
+import subprocess
+pathModel = "vosk-model-en-us-daanzu-20200905-lgraph"
+voskModel = Model(pathModel)
+rec       = KaldiRecognizer(voskModel, 16000)
+
+CHUNK    = 16000
+FORMAT   = pyaudio.paInt16
+CHANNELS = 1
+RATE     = 16000
+RECORD_SECONDS = 60*60*24
+data = b""
+HOW_MANY_SECONDS_TO_RECOGNIZE = 3
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+for i in range(1, int(RATE / CHUNK * RECORD_SECONDS)+1):
+    data += stream.read(CHUNK)
+    
+    rec.AcceptWaveform(data)
+    print(getText(rec.FinalResult()))
+    
+    if((i % HOW_MANY_SECONDS_TO_RECOGNIZE) == 0): data = b""
+    
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
