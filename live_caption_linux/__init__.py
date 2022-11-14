c77979759bbcae04bc969f5a13cfcfe64902cb3c
RonyMacfly <rony.macfly@gmail.com>
2022-11-14 16:44:18 -0500

    setup.py

---
+++ live_caption_linux/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.0"
+
+from live_caption_linux import recognizeAudioStream
+
+def init():
+    recognizeAudioStream.start()
---
+++ tests/test_init.py
@@ -0,0 +1,4 @@
+
+from live_caption_linux import init
+
+init()
