b728c30bdec5dd5742ef5b15dc6ac89fa0be6aa5
RonyMacfly <rony.macfly@gmail.com>
2022-11-15 14:55:26 -0500

    include_package_data

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     author='Rony Macfly',
     author_email='rony.macfly@gmail.com',
     packages=find_packages(),
+    package_data = {
+        '': ['*'],
+    },
+    include_package_data=True,
     install_requires=['pyaudio',],
     entry_points={
         'console_scripts': [
